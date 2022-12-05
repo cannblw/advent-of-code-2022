37bb4b6de62a952c7b1bb4fabaa7d809d6eb1d56
Edgar Chirivella <edgar.95.12@gmail.com>
2022-12-05 18:11:42 +0100

    Simplify solution

--- 01-calorie-counting/index.js
+++ 01-calorie-counting/index.js
@@ -3,7 +3,6 @@ console.log(
     .readFileSync('./input.txt')
     .toString()
     .split('\n\n')
-    .map(x => x.split('\n'))
-    .map(x => x.reduce((p, c) => p + +c, 0))
+    .map(x => x.split('\n').reduce((p, c) => p + +c, 0))
     .reduce((p, c) => c > p ? c : p, 0)
 );
